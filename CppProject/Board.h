ead327d6620befb0cf77e902c9fbeafa2b6cc2dc
Cathyryn <caseoryn@gmail.com>
2026-05-25 02:13:20 +0900

    feat: 2단계 뱀 이동 로직 및 충돌 판정 구현

---
+++ CppProject/Board.h
@@ -0,0 +1,27 @@
+/*
+ * File: Board.h
+ * Description: Board 클래스의 선언부. 게임 맵(스테이지) 데이터 유지 및 화면 렌더링
+ * Author: 박서린
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <deque>
+#include "Snake.h"
+
+const int MAP_SIZE = 21;
+
+class Board {
+private:
+    int stageData[MAP_SIZE][MAP_SIZE];
+
+public:
+    Board();
+
+    void loadStage(const int stageIndex);
+    void draw(const std::deque<Pos>& snakeBody, const int current_len, const int max_len) const;
+    bool checkWallCollision(const Pos p) const;
+};
+
+#endif
